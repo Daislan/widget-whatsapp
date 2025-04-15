49f3d157b0c8f5a214c494d0bfde4f0edd42c74c
Daislan <daislannogueiratec@gmail.com>
2025-04-15 14:45:17 -0300

    .

--- widget-whatsapp.js
+++ widget-whatsapp.js
@@ -17,9 +17,9 @@
     input.type = field.type;
     input.required = field.required;
     input.style.width = '100%';
-    input.style.padding = '20px';
+    input.style.padding = '8px';
     input.style.border = '1px solid #ccc';
-    input.style.borderRadius = '8px';
+    input.style.borderRadius = '50px';
     input.style.marginBottom = '10px';
 
     if (field.name === 'whatsapp') {
